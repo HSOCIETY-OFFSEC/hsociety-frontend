854fca447824a93fd56d2ccdfdf78de1b408b9d8
wsuits6 <wsuits6@gmail.com>
2026-01-31 12:50:36 +0000

    updated main.tsx to import component styles CSS

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
+// src/main.jsx
+
+/**
+ * Application Entry Point
+ * Updated with component styles import
+ */
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { ThemeProvider } from './context/ThemeContext.jsx';
 import './index.css';
 import './styles/theme.css';
+import './styles/components.css';
 import './App.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
